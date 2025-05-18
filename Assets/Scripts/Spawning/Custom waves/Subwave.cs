ba1a028f008a924b44e842d0979d76fdabe2cab2
DOOT <jori-kos@hotmail.com>
2025-05-18 14:36:55 +0200

    Set up json-based waves instead

---
+++ Assets/Scripts/Spawning/Custom waves/Subwave.cs
@@ -0,0 +1,10 @@
+using System;
+
+[Serializable]
+public class Subwave
+{
+    //Things to add:
+    //Amount of the different enemy types
+    //Total amount of value from the wave (distributed over all the different enemies in script, higher lvl enemy = more value assigned)
+    public float _timeBeforeSubWaveStart, _enemiesInSubWave;
+}
